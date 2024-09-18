d00e2ae52f3a6c6361e9ad6a4beb399f884e4022
Temesgen Asmamaw <asmamawtemesgen16@gmail.com>
2024-09-18 11:46:16 +0300

    furthur preprocess

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -2,10 +2,13 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler,OneHotEncoder
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder
 
-def load_and_clean_data(filepath):
-    data = pd.read_csv(filepath)
+def load_and_clean_data(data):
     # Removing duplicates
     data = data.drop_duplicates(keep="first")
     return data
@@ -30,9 +33,11 @@ def title_to_gender_map(df,title_to_gender_map):
     df['Gender'] = df.apply(lambda row: title_to_gender_map.get(row['Title'], row['Gender']), axis=1)
 
     # Display the updated Gender counts
+    return df
     print(df['Gender'].value_counts())
    
 def encoder(method, dataframe, columns_label, columns_onehot):
+    
     if method == 'labelEncoder':      
         df_lbl = dataframe.copy()
         for col in columns_label:
@@ -46,6 +51,27 @@ def encoder(method, dataframe, columns_label, columns_onehot):
         df_oh= pd.get_dummies(data=df_oh, prefix='ohe', prefix_sep='_',
                        columns=columns_onehot, drop_first=True, dtype='int8')
         return df_oh
+
+
+def column_encoder(df,label_encode_cols,one_hot_encode_cols,frequency_encode_cols):
+    print()
+
+    # 1. Label Encoding for low cardinality columns
+    label_encoders = {}
+    for feature in label_encode_cols:
+        le = LabelEncoder()
+        df[feature] = le.fit_transform(df[feature])
+        label_encoders[feature] = le
+
+    # One-Hot Encoding for moderate cardinality features
+    # 2. One-Hot Encoding for moderate cardinality columns
+    df = pd.get_dummies(df, columns=one_hot_encode_cols, drop_first=True)
+    # 3. Frequency Encoding for high cardinality columns
+    for col in frequency_encode_cols:
+        freq = df[col].value_counts()
+        df[col + '_freq'] = df[col].map(freq)
+        df = df.drop(columns=[col])
+    return df
 
 def scaler(method, data, columns_scaler):    
     if method == 'standardScaler':        
